dfa6701c5e789b80f7ebd0b6a998fc83655bfcc3
Nabneet101 <Nabneet101@github.com>
2023-10-19 07:31:58 +0530

    First Commit Project Added

---
+++ testGitSts/src/main/java/com/test/git/controller/controllerdf.java
@@ -0,0 +1,19 @@
+package com.test.git.controller;
+
+public class controllerdf {
+ String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+@Override
+public String toString() {
+	return "controllerdf [name=" + name + "]";
+}
+ 
+}
